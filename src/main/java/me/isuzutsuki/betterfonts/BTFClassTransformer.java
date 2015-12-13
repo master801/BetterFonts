2880148dbf50dfc6426071ad4bcfd4c2b1731d7c
master801 <master801@users.noreply.github.com>
2015-12-13 05:31:53 -0600

    Some work

--- src/main/java/me/isuzutsuki/betterfonts/BTFClassTransformer.java
+++ src/main/java/me/isuzutsuki/betterfonts/BTFClassTransformer.java
@@ -1,56 +1,36 @@
 package me.isuzutsuki.betterfonts;
 
-import net.md_5.jbeat.PatcherIO;
 import net.minecraft.launchwrapper.IClassTransformer;
+import org.slave.lib.asm.transformers.substitution.SubstitutionTransformer;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+public final class BTFClassTransformer extends SubstitutionTransformer implements IClassTransformer {
 
-public final class BTFClassTransformer implements IClassTransformer {
-
-//    private static final String PATCH_FILE_PATH = "/patch/FontRenderer.java.patch";
-    private static final String PATCH_FILE_PATH = "/patch/FontRenderer.java.bps";
-
-    /**
-     * @param obfClassName Obfuscated class name
-     * @param className Deobfuscated name (usually the Searge class name)
-     * @param bytes The class' data.
-     *
-     * @return The data for the class.
-     */
-    @Override
-    public byte[] transform(String obfClassName, String className, final byte[] bytes) {
-        if (bytes == null || bytes.length <= 0) return null;//Check if the class data is valid.
-        byte[] returnData = bytes;
-        if (className.equals("net.minecraft.client.gui.FontRenderer")) {
-            BetterFontsCore.BETTER_FONTS_LOGGER.info("Patching class \"{}\"...", arg0);
-            returnData = beatPatch(className, bytes);
-        }
-        if (returnData == null || returnData.length <= 0) {//Patching failed
-            returnData = bytes;
-            BetterFontsCore.BETTER_FONTS_LOGGER.error("Failed to patch class \"{}\".");
-        }
-        return returnData;
+    public BTFClassTransformer() {
+        super(BetterFontsCore.BETTER_FONTS_LOGGER);
     }
 
-    private byte[] beatPatch(final String className, final byte[] data) {
-        InputStream patchFileInputStream = BTFClassTransformer.class.getResourceAsStream(BTFClassTransformer.PATCH_FILE_PATH);
-        if (patchFileInputStream == null) {
-            BetterFontsCore.BETTER_FONTS_LOGGER.error("Failed to find patch file \"{}\"!", BTFClassTransformer.PATCH_FILE_PATH);
-            return data;
+    @Override
+    protected String getSubstitutionClassPath() {
+        return Substitution.class.getName();
+    }
+
+    @Override
+    protected String getClassName(TransformationType transformationType) {
+        switch(transformationType) {
+            case UNTRANSFORMED:
+                return "bty";
+            case TRANSFORMED:
+            default:
+                return "net.minecraft.client.gui.FontRenderer";
         }
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PatcherIO patcherIO = new PatcherIO(new ByteArrayInputStream(data), patchFileInputStream, byteArrayOutputStream);
-        try {
-            patcherIO.patch();
-        } catch (IOException e) {
-            BetterFontsCore.BETTER_FONTS_LOGGER.error("Caught exception \"{}\" when attempting to patch class \"{}\"!", e, className);
-            return data;
-        }
-        BetterFontsCore.BETTER_FONTS_LOGGER.info("Patched class \"{}\"!", className);
-        return byteArrayOutputStream.toByteArray();
+    }
+
+    @Override
+    public byte[] transform(String s, String s1, byte[] bytes) {
+        return super.transform(bytes, s, s1);
+    }
+
+    private static final class Substitution {
     }
 
 }
